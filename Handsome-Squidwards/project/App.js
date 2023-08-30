ebb40ffbb9fe6bfddef11f09430a3f1143c26a97
Золбаяр Дэлгэрбаяр <HP233061@M301-CC0136.local>
2023-08-30 13:47:51 +0800

    comment post

--- Handsome-Squidwards/project/App.js
+++ Handsome-Squidwards/project/App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, SafeAreaView, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { Post } from './components/Post'
+import { createStackNavigator } from '@react-navigation/stack';
+import { Comments } from './components/Comments';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MyStack/>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function MyStack () {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name='Post' component={Post} />
+      <Stack.Screen name="Comments" component={Comments} />
+    </Stack.Navigator>
+  );
+}
