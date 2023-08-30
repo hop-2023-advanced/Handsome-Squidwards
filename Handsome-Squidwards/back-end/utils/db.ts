8b49a9148891b5b2a438d8036f07eaa676308102
Erdemeesgithub <berdemdelgerekh@gmail.com>
2023-08-30 14:18:09 +0800

    reverse wrong deletion, fix gitignore

---
+++ Handsome-Squidwards/back-end/utils/db.ts
@@ -0,0 +1,23 @@
+import fetch from "node-fetch";
+
+export default async function handler(action: any, options: any) {
+  const result = await fetch(
+    `https://ap-southeast-1.aws.data.mongodb-api.com/app/data-mophc/endpoint/data/v1/action/${action}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "api-key":
+          "ZdjeUWVwjTOjEqdqp35SF0NEI9Da6TxCEtzVi1JkErTSock8PoqsAgo26ZuWLGRo",
+      },
+      body: JSON.stringify({
+        dataSource: "Cluster0",
+        database: "InstagramDb",
+        collection: "Usernames",
+        ...options,
+      }),
+    }
+  ).then((res) => res.json());
+
+  return result;
+}
